1201945514f72d331563488d3e499b4f2881422a
unknown <P11872@P11872.SCM.PANTECH.COM>
2015-08-10 12:40:31 +0900

    [kim jung sick] Profile server

---
+++ server/web_test/putprofile.php
@@ -0,0 +1,51 @@
+<?php
+
+function putProfile(){
+	$user_id = $_POST ['user_id'];
+	$name = $_POST ['user_name'];
+	$email = $_POST ['user_email'];
+	$gender = $_POST ['user_gender'];
+	$score = $_POST ['user_score'];
+	$bookmark = $_POST ['user_bookmark'];
+	$dbname = 'db_chat_member_test';
+
+	$sql_query = "insert into member (user_id, user_name, user_email, user_gender, user_score, user_bookmark) values('$user_id', '$name', '$email','$gender','$score', '$bookmark')";
+
+	$mysqli = new mysqli ( "localhost", "root", "111111", $dbname );
+	if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+
+	$create_table = "create table if not exists profiles(
+						id int primary key auto_increment,
+						user_id varchar(12) unique,
+						name varchar(10),
+						email varchar(20),
+						gender varchar(10),
+						score varchar(10),
+						bookmark varchar(20)
+						);";
+	
+	$mysqli->query ( $create_table );
+
+	$mysqli->query ( $sql_query );
+
+	if ($mysqli->insert_id) {
+		echo 'insert success';
+		$value = "success";
+	} else {
+		echo 'insert fail';
+		$value = "insert fail";
+	}
+
+	$mysqli->close ();
+	return $value;
+}
+
+$value = "An error has occurred";
+
+if (isset ( $_POST ["user_id"])){
+	$value = putProfile();
+}
+exit ( json_encode ( $value ) );
+?>
